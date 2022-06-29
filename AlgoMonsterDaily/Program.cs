1ab5a1c9575ae1855a7a7f912d0c06675a4ad1d9
e-man <tenhitcombo@gmail.com>
2022-06-29 16:33:27 -0400

    separated bst to its own class.

--- AlgoMonsterDaily/Program.cs
+++ AlgoMonsterDaily/Program.cs
@@ -4,34 +4,6 @@ using System.Linq;
 
 namespace AlgoMonsterDaily
 {
-    public class BST
-    {
-        public static bool ValidBST(Node<int> root)
-        {
-            return DFS(root, Int32.MinValue, Int32.MaxValue);
-        }
-       
-        public static bool DFS(Node<int> root, int min, int max)
-        {
-            // empty nodes always return true
-            if (root == null) return true;
-            if (!(min <= root.val && root.val <= max)) return false;
-
-            return DFS(root.left, min, root.val) && DFS(root.right, root.val, max); 
-        }
-
-        public static Node<int> InsertBST(Node<int> bst, int val)
-        {
-            if (bst == null) return new Node<int>(val);
-            int compareVal = bst.val.CompareTo(val);
-            if (compareVal < 0) bst.right = InsertBST(bst.right, val);
-            else if (compareVal > 0) bst.left =  InsertBST(bst.left, val);
-
-            return bst;
-        }        
-    }
-
-
     class Program
     {
         public static void Main()
