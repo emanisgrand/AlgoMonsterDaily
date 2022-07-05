2735e45e00e2846839338ce49ce7e2c28600d54e
e-man <tenhitcombo@gmail.com>
2022-07-05 19:27:51 -0400

    wrote a real quick modulo function

--- AlgoMonsterDaily/Program.cs
+++ AlgoMonsterDaily/Program.cs
@@ -11,13 +11,22 @@ namespace AlgoMonsterDaily
             IList<int> list = new List<int>();
             return list;
         }
+
+        public static int Mod(int x, int y)
+        {
+            while (x >= y)
+            {
+                x -= y;
+            }
+            return x;
+        }
         public static void Main()
         {
             //List<string> strs = DFS.SplitWords("1 2 4 x 7 x x 5 x x 3 x 6 x x");
             List<int> strs = new List<int>{1, 2, 3, 3, 3, 6, 6, 6, 10, 12, 189};
             int target = 6;
             //TreeNode<int> root = DFS.BuildTree(strs, ref pos, int.Parse);
-            var res = BinarySearch.SquareRoot(16);
+            var res = Mod(100, 27);
             Console.WriteLine(res);
          }
     }
