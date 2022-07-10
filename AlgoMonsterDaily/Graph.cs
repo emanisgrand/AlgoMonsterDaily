09323eb4cf138a24419d6624cd87bcd5c4c90dad
e-man <tenhitcombo@gmail.com>
2022-07-09 20:44:47 -0400

    created alts to bfs to work w/o collections

--- AlgoMonsterDaily/Graph.cs
+++ AlgoMonsterDaily/Graph.cs
@@ -8,9 +8,74 @@ namespace AlgoMonsterDaily
 {
     internal class Graph
     {
+        public static int[,] AryFloodFill(int r, int c, int replacement, int[,] image)
+        {
+            int numRows = 5;
+            int numCols = 5;
+
+            aryBfs(image, new Utils.Coordinate(r, c), replacement, numRows, numCols);
+            return image;
+        }
+
+        private static void aryBfs(int[,] image, Utils.Coordinate root, int replacementColor, int numRows, int numCols)
+        {
+            Queue<Utils.Coordinate> queue = new Queue<Utils.Coordinate>();
+            queue.Enqueue(root);
+
+            bool[,] visited = new bool[numRows, numCols];
+            
+            int rootColor = image[root.r,root.c];
+
+
+            image[root.r,root.c] = replacementColor;
+
+            visited[root.r, root.c] = true;
+
+            while (queue.Count > 0)
+            {
+                Utils.Coordinate node = queue.Dequeue();
+
+                List<Utils.Coordinate> neighbors = AryGetNeighbors(image, node, rootColor, numRows, numCols);
+
+                foreach (Utils.Coordinate neighbor in neighbors)
+                {
+
+                    if (visited[neighbor.r, neighbor.c]) continue;
+
+                    image[neighbor.r,neighbor.c] = replacementColor;
+
+                    queue.Enqueue(neighbor);
+
+                    visited[neighbor.r, neighbor.c] = true;
+
+                }
+            }
+        }
+        private static List<Utils.Coordinate> AryGetNeighbors(int[,] image, Utils.Coordinate node, int rootColor, int numRows, int numCols)
+        {
+            List<Utils.Coordinate> neighbors = new List<Utils.Coordinate>();
+
+            int[] deltaRow = { -1, 0, 1, 0 };
+            int[] deltaCol = { 0, 1, 0, -1 };
+
+            for (int i = 0; i < deltaRow.Length; i++)
+            {
+
+                int neighborRow = node.r + deltaRow[i];
+                int neighborCol = node.c + deltaCol[i];
+
+                if (0 <= neighborRow && neighborRow < numRows && 0 <= neighborCol && neighborCol < numCols)
+                {
+                    if (image[neighborRow,neighborCol] == rootColor)
+                    {
+                        neighbors.Add(new Utils.Coordinate(neighborRow, neighborCol));
+                    }
+                }
+            }
+            return neighbors;
+        }
         public static List<List<int>> floodFill(int r, int c, int replacement, List<List<int>> image)
         {
-            // square image
             int numRows = image.Count;
             int numCols = image[0].Count;
 
@@ -18,7 +83,6 @@ namespace AlgoMonsterDaily
 
             return image;
         }
-
         private static void bfs(List<List<int>> image, Utils.Coordinate root, int replacementColor, int numRows, int numCols)
         {
 
--- AlgoMonsterDaily/Program.cs
+++ AlgoMonsterDaily/Program.cs
@@ -6,22 +6,35 @@ namespace AlgoMonsterDaily
 {
     class Program
     {
+        public void PrintPreImage(int[,] preImage)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0} ", preImage[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
         public static void Main()
         {
+            Program pr = new Program();
+            int[,] preImage = { {0, 1, 3, 4, 1 },
+                            {3, 8, 8, 3, 3 },
+                            {6, 7, 8, 8, 3 },
+                            {12, 2, 8, 9, 1 },
+                            {12, 3, 1, 3, 2 },
+            };
+            pr.PrintPreImage(preImage);
             int r = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
             int replacement = int.Parse(Console.ReadLine());
-            int imageLength = int.Parse(Console.ReadLine());
-            List<List<int>> image = new List<List<int>>();
-            for (int i = 0; i < imageLength; i++)
-            {
-                image.Add(Utils.SplitWords(Console.ReadLine()).Select(int.Parse).ToList());
-            }
-            List<List<int>> res = Graph.floodFill(r, c, replacement, image);
-            foreach (List<int> row in res)
-            {
-                Console.WriteLine(String.Join(' ', row));
-            }
+            //int imageLength = int.Parse(Console.ReadLine());
+            //List<List<int>> image = new List<List<int>>();
+            
+            int[,] res = Graph.AryFloodFill(r, c, replacement, preImage);
+            pr.PrintPreImage(preImage);
         }
     }
 }
