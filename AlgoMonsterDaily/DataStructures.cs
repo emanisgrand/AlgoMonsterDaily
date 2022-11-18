1256aa88f3904d6eb711f6ebccee844d226ec60d
e-man <tenhitcombo@gmail.com>
2022-11-17 22:46:42 -0500

    DFS permutations with test

--- AlgoMonsterDaily/DataStructures.cs
+++ AlgoMonsterDaily/DataStructures.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    public class LinkedLRUCache : Dictionary<int, LinkedListNode<int[]>>
+    public class LRUCache : Dictionary<int, LinkedListNode<int[]>>
     {
-        uint _capacity;
+        int _capacity;
         LinkedList<int[]> _list = new LinkedList<int[]>();
-        public LinkedLRUCache(uint capacity):base((int)capacity)
+        public LRUCache(int capacity):base(capacity)
         {
             _capacity = capacity;
         }
@@ -48,48 +48,6 @@ namespace DataStructures
 
             if(_list.First != node)
                 _list.AddFirst(node);
-        }
-    }
-
-    public class LRUCache : Dictionary<int, int>
-    {
-        uint _capacity;
-        public LRUCache(uint capacity) : base((int)capacity)
-        {
-            _capacity = capacity;
-        }
-
-        public int Get(int key)
-        {
-            if (base.TryGetValue(key, out int value))
-            {
-                return value;
-            }
-            else
-            {
-                return -1;
-            }
-        }
-
-        public void Put(int key, int value)
-        {
-            if (this.ContainsKey(key))
-            {
-                this[key] = value;
-            }else
-            {
-                this.Add(key, value);
-            }
-        }
-
-        public int Get(int key)
-        {
-            return -1;
-        }
-
-        public void Put(int key, int value)
-        {
-            
         }
     }
 
---
+++ AlgoTests/LeetCode_LRUCacheTests.cs
@@ -0,0 +1,117 @@
+using AlgoMonsterDaily;
+using Algorithms.TwoPointers;
+using DataStructures;
+using System.Data;
+
+namespace AlgoTests
+{
+    [TestClass]
+    public class LeetCode_LRUCacheTests
+    {
+        [TestMethod]
+        [DataRow(2)]
+        [DataRow(5)]
+        [DataRow(20)]
+        [DataRow(2000)]
+        public void LRUCache_RandomValues_OverFitting(int capacity)
+        {
+            var cache = new LRUCache(capacity);
+            int outlier = 0;
+            for (int i = 1; i < capacity; i++)
+            {
+                int r = new Random(i % capacity * new Random().Next(99)).Next(99);
+                cache.Put(i, r);
+                Assert.AreEqual(cache.Get(i), r);
+                outlier = i++;
+            }
+
+            cache.Put(outlier, outlier + 7);
+            Assert.AreEqual(cache.Get(2), -1);
+            cache.Put(4, 16);
+            cache.Put(5, 25);
+            cache.Put(8, 64);
+            //Assert.AreEqual(cache.Get(outlier), outlier+7);
+            //Assert.AreEqual(cache.Get(4), 8);
+            Assert.AreEqual(cache.Get(20), -1);
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        [DataRow(5)]
+        [DataRow(20)]
+        [DataRow(2000)]
+        public void LRUCache_OverFitting(int capacity)
+        {
+            var cache = new LRUCache(capacity);
+            int outlier = 0;
+            for (int i = 1; i < capacity; i++)
+            {
+                cache.Put(i, i);
+                Assert.AreEqual(cache.Get(i), i);
+                outlier = i++;
+            }
+
+            cache.Put(outlier, outlier + 7);
+            Assert.AreEqual(cache.Get(2), -1);
+            cache.Put(4, 16);
+            cache.Put(5, 25);
+            cache.Put(8, 64);
+            //Assert.AreEqual(cache.Get(outlier), outlier+7);
+            //Assert.AreEqual(cache.Get(4), 8);
+            Assert.AreEqual(cache.Get(20), -1);
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        [DataRow(20)]
+        [DataRow(2000)]
+        public void LRUCache_GetFunctionReturnsKey(int capacity)
+        {
+            var cache = new LRUCache(capacity);
+            int outlier = 0;
+            for (int i = 1; i < capacity; i++)
+            {
+                cache.Put(i, i);
+                Assert.AreEqual(cache.Get(i), i);
+                outlier = i++;
+            }
+            
+            cache.Put(outlier, outlier+7);
+            Assert.AreEqual(cache.Get(2), -1);
+            cache.Put(4, 8);
+            //Assert.AreEqual(cache.Get(outlier), outlier+7);
+            Assert.AreEqual(cache.Get(4), 8);
+            Assert.AreEqual(cache.Get(20), -1);
+        }
+
+        [TestMethod]
+        public void LRUCache_()
+        {
+            var cache = new LRUCache(2);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.AreEqual(cache.Get(1), 1);
+        }
+        [TestMethod]
+        public void LRUCacheCapacityCanBeSet()
+        {
+            var cache = new LRUCache(2);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.AreEqual(cache.Get(1), 1);
+            cache.Put(3, 3);
+            Assert.AreEqual(cache.Get(1), -1);
+            Assert.AreEqual(cache.Get(3), 3);
+        }
+
+        [TestMethod]
+        public void LRUCacheRejectsInputsThatArentInTheCache()
+        {
+            var cache = new LRUCache(2);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.AreEqual(cache.Get(3), -1);
+
+        }
+    }
+}
