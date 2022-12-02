e47d5b8b914f1d1539523187f429e1ff3947804c
emanisgrand <tenhitcombo@gmail.com>
2022-12-02 10:17:27 -0500

    gfgTest FindTripletsWithZeroSum cases and result added.

--- AlgoTests/GFGTests.cs
+++ AlgoTests/GFGTests.cs
@@ -7,11 +7,13 @@ namespace AlgoTests
         /// Given an array of distinct elements. The task is to find triplets in the array whose sum is zero.
         /// </summary>
         [TestMethod]
-        [DataRow("0 -1 2 -3 1")]
-        public void FindTriplestWithZeroSum(string data)
+        [DataRow("0 -1 2 -3 1", new int[3] {0, -1, 1})]
+        [DataRow("0 -1 2 -3 1", new int[3] {2, -3, 1})]
+        public void FindTriplestWithZeroSum(string data, int[] expected)
         {
             var arr = Helpers.SplitWords(data).Select(int.Parse).ToArray();
-            Assert.AreEqual(arr[1], -1);
+            
+
         }
     }    
 }
