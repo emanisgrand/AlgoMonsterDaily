e6ca24a673611d87eb5a9ffaf6afeda1c274170f
e-man <tenhitcombo@gmail.com>
2022-12-16 17:05:47 -0500

    islands prompted. error cleaned.

--- AlgoMonsterDaily/Graphs.cs
+++ AlgoMonsterDaily/Graphs.cs
@@ -98,58 +98,40 @@ namespace Graphs
     }
 
     public static class NumberOfIslands
-    {// PROMPTED: ✅✅❌❌
+    {// PROMPTS: 
         public static int BFS(char[][] grid)
         {
-            int islands = 0;/*🌟*/
+            int islands = 0;
 
-            int[] deltaCol = { 0, 0, 1, -1 };/*🧟*/
-            int[] deltaRow = { 1, -1, 0, 0};/*🧟*/
-
-            // 0. start iterating through rows and columns
-            for (int r =0; r<grid.Length; r++)
-            {
-                for (int c = 0; c < grid[0].Length; c++)/*👣*/
-                {
-                    // 1. if the row/col is an island
+            int[] deltaRow = new int[] { 1, -1, 0, 0 };
+            int[] deltaCol = new int[] { 0, 0, 1, -1 };
+            
+            for (int r = 0; r<grid.Length; r++) {
+                for (int c = 0; c < grid[0].Length; c++) {
                     if (grid[r][c] == '1')
                     {
-                        // 1. increment the number of islands!
                         islands++;
 
-                        // 2. initialize a Q for the r,c pairs 
                         Queue<int[]> Q = new Queue<int[]>();
-
-                        // 3. enqueue the currently visited r c pair
-                        Q.Enqueue(new int[] { r,c});
-
-                        // 4. change the value at that grid to mark it as visited?
+                       
+                        Q.Enqueue(new int[] { r,c });
                         grid[r][c] = 'X';
 
-                        // 5. while the queue is not empty
-                        while(Q.Count > 0)
+                        while (Q.Count > 0)
                         {
-                            // 1. take a look at the r,c pair 
-                            int[] rc = Q.Dequeue();
- 
-                            // 2. loop to scan all 4 sides
+                            int[] cur = Q.Dequeue();
+
                             for (int i=0; i<4; i++)
                             {
-                                // 1. store each row and col neighbor as we iterate
-                                int neibR = rc[0] + deltaRow[i];
-                                int neibC = rc[1] + deltaCol[i];
-                                // 2. check to make sure that we're in bounds...
+                                int neibR = cur[0] + deltaRow[i];
+                                int neibC = cur[1] + deltaCol[i];
+                                
                                 if (neibR > -1 && neibR < grid.Length &&
                                     neibC > -1 && neibC < grid[0].Length &&
-                                // is not water...
-                                grid[neibR][neibC] != '0' &&
-                                // and has not already been visited.
-                                grid[neibR][neibC] != 'X'
-                                )
+                                    grid[neibR][neibC] != '0' && 
+                                    grid[neibR][neibC] != 'X')
                                 {
-                                    // 1. queue up the thoroughly visited cell
                                     Q.Enqueue(new int[] { neibR, neibC });
-                                    // 2. mark this cell as visited
                                     grid[neibR][neibC] = 'X';
                                 }
                             }
@@ -158,8 +140,8 @@ namespace Graphs
                 }
             }
 
-
-            return islands; 
+            // return the number of islands.
+            return islands;
         }
     }
 }
