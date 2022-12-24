bdb5b0c4cb20ca3e4128d1af7f95e8b9738a2403
e-man <tenhitcombo@gmail.com>
2022-12-24 14:45:55 -0500

    Summary added. Done blind.

--- AlgoMonsterDaily/BinarySearch.cs
+++ AlgoMonsterDaily/BinarySearch.cs
@@ -4,44 +4,35 @@ namespace Binary
 {
     public static class RotateSorted
     {
+        /// <summary>
+        /// Determine potential pivot point of values then compare to find minimum
+        /// </summary>
+        /// <param name="nums">Integer array, possibly rotated.</param>
+        /// <returns>Int: minimum value in rotated array</returns>
         public static int FindMin(int[] nums)
         {
-            // something to store the result
             int res = nums[0];
-            // left and right pointers
             int left = 0, right = nums.Length - 1;
-            
-            // while left is <= right
+
             while (left <= right)
             {
-                // check both ends of the array to see if it's in sorted order.
-                // if it is, return the minimum of current result value and 
-                // value that is on the leftmost side of the array
-                if (nums[left] < nums[right]) return Math.Min(res, nums[left]);
-                
-                // compute the mid pointer now that we'll be doing binary search
+                if (nums[left] < nums[right]) return Math.Min(nums[left], res);
+
                 int mid = (left + right) / 2;
 
-                // update result variable with the minimum between current result and current value at mid
-                // it could be the pivot point and thus the smallest value.
-                res = Math.Min(res, nums[mid]);
+                res = Math.Min(nums[mid], res);
 
-                // is mid value part of left sorted portion?
-                if (nums[mid] >= nums[left])
-                // then update left so we can scan right side
+                if (nums[left] <= nums[mid])
                 {
-                    left = mid + 1;
+                    left = mid + 1; 
                 }
-                // else
                 else
                 {
-                    // update right so we can scan left side
                     right = mid - 1;
                 }
-                // return result.
             }
-            
-            return 0;
+
+            return res;
         }
 
         /// <summary>
